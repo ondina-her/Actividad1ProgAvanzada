eee754056d255a2b0e923dc219eb6b1c43f34ac4
ondina-her <158087862+ondina-her@users.noreply.github.com>
2025-03-09 23:15:41 -0600

    Add files via upload
    add files

---
+++ app/habits-tracker/app/habits.tsx
@@ -0,0 +1,40 @@
+/*type Habits = {
+     title: string;
+     description: string;
+    }
+    type HabitsProps = {
+     habits: Habits[];
+    }
+    export default function Habits({habits}: HabitsProps) {
+     return (
+     <ul>
+     {habits.map((habit) => (
+   <li key={habit.title}>{habit.title}</li>
+     ))}
+     </ul>
+     );
+     
+}
+*/
+
+   
+
+  type Habit = {
+      title: string;
+      description: string;
+      }
+      type HabitsProps = {
+      habits: Habit[];
+      }
+      export default function Habits({habits}: HabitsProps) {
+      console.log('habits', habits)
+      return (
+      <ul>
+      {
+        habits?.map((habit:Habit) => (
+      <li key={habit.title}>{habit.title}</li>
+      ))}
+      </ul>
+      );
+      }
+      
